c00976cafcc643ea7010ffd6162f85abd9dfd35c
unknown <bubbastinx@gmail.com>
2018-03-27 20:42:44 -0600

    initialized firebase

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+'strict'
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDMf2J88v0vsnt0ctwPWH5S5kgNgn2iczQ",
+    authDomain: "trainscheduleproject-70841.firebaseapp.com",
+    databaseURL: "https://trainscheduleproject-70841.firebaseio.com",
+    projectId: "trainscheduleproject-70841",
+    storageBucket: "",
+    messagingSenderId: "971069667233"
+  };
+  firebase.initializeApp(config);
